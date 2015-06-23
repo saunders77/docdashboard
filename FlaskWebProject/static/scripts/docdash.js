de1c8a6fa628b5d46d39c66653c55a77225c6265
saunders77 <michaelsaunders@outlook.com>
2015-06-23 14:58:45 -0700

    scripts initialization

---
+++ FlaskWebProject/static/scripts/docdash.js
@@ -0,0 +1,17 @@
+﻿function write(myText) {
+    document.getElementById("debug2").innerHTML += "<BR>" + myText;
+}
+
+/*
+Office.initialize = function (reason) {
+    // might need to record somewhere if we haven't loaded it
+    */
+
+    $(document).ready(function () {
+
+
+        window.alert("Hello World!");
+
+    });
+/*} */
+
