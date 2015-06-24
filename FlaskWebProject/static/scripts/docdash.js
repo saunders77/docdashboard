115483eabe81bfb1f0cb53507860cd7ce0416a74
saunders77 <michaelsaunders@outlook.com>
2015-06-24 14:08:44 -0700

    undeserialization

--- FlaskWebProject/static/scripts/docdash.js
+++ FlaskWebProject/static/scripts/docdash.js
@@ -163,7 +163,7 @@ function post() {
 }
 
 function postCallback(result){
-    write("docid from Post is: " + JSON.parse(result).docid);
+    write("docid from Post is: " + result.docid);
 }
 
 Office.initialize = function (reason) {
