4bfe92cdd70ac9d6a4dc88ae86326b7a57b012b3
saunders77 <michaelsaunders@outlook.com>
2015-06-24 13:21:03 -0700

    no this

--- FlaskWebProject/static/scripts/docdash.js
+++ FlaskWebProject/static/scripts/docdash.js
@@ -28,48 +28,48 @@ var myDoc = {
         // pull in data from the document, if any
         var result = false;
         if(Office.context.document.settings.get("data")){
-            this.data = Office.context.document.settings.get("data");
+            myDoc.data = Office.context.document.settings.get("data");
             result = true;
         }
         return result;
     },
     
     saveStateToFile: function(){
-        Office.context.document.settings.set("data",this.data);
-        Office.context.document.settings.saveAsync("data",this.data);
+        Office.context.document.settings.set("data", myDoc.data);
+        Office.context.document.settings.saveAsync("data", myDoc.data);
     },
     
     startRecording: function(){
         //write("started recording");
         var result = true;
-        if (this.isRecording) {
+        if (myDoc.isRecording) {
             // then you can't start it if it's already started
             result = false;
         }
         else{
             //write("hasn't started yet");
-            this.isRecording = true;
+            myDoc.isRecording = true;
             //write("hasn't started yet2");
-            if (!this.data.stats.timeCreated) {
+            if (!myDoc.data.stats.timeCreated) {
                 // then this is the first time recording has ever happened in this doc
                 var d = new Date();
-                this.data.stats.timeCreated = d.getTime();
+                myDoc.data.stats.timeCreated = d.getTime();
             }
             //write("hasn't started yet3");
-            this.recordNextStats();
+            myDoc.recordNextStats();
         }
         return result;
     },
     
     recordNextStats: function(){
         write("recording next stats");
-        Office.context.document.getFileAsync("text",this.gotFullText);
+        Office.context.document.getFileAsync("text", myDoc.gotFullText);
         
         // now trigger the next recording, if necessary
-        if (this.isRecording) {
-            this.recordingTimeout = setTimeout(
-                this.recordNextStats,
-                this.data.recordingPeriod
+        if (myDoc.isRecording) {
+            myDoc.recordingTimeout = setTimeout(
+                myDoc.recordNextStats,
+                myDoc.data.recordingPeriod
             );
         }
         
@@ -80,40 +80,40 @@ var myDoc = {
         if (result.status == "succeeded") {
             var d = new Date();
             write("length: " + result.value.length);
-            this.data.stats.charCount = result.value.length;
-            this.data.stats.charCounts.push([d.getDate(),this.data.stats.charCount]);
+            myDoc.data.stats.charCount = result.value.length;
+            myDoc.data.stats.charCounts.push([d.getDate(), myDoc.data.stats.charCount]);
         }
         else{
             write("Error:", result.error.message);
         }
         
-        this.saveStateToFile();
+        myDoc.saveStateToFile();
     },
     
     startDisplaying: function(){
         var result = true;
         write("start?  displaying");
-        if (this.isDisplaying) {
+        if (myDoc.isDisplaying) {
             // then you can't start it if it's already started
             result = false;
         }
         else{
             write("yes, we can start displaying");
-            this.isDisplaying = true;
-            this.displayNextStats();
+            myDoc.isDisplaying = true;
+            myDoc.displayNextStats();
         }
         return result;
     },
     
     displayNextStats: function(){
         
-        write("Characters: " + this.data.stats.charCount);
+        write("Characters: " + myDoc.data.stats.charCount);
         
         // now trigger the next recording, if necessary
-        if (this.isDisplaying) {
-            this.displayingTimeout = setTimeout(
-                this.displayNextStats,
-                this.data.displayingPeriod
+        if (myDoc.isDisplaying) {
+            myDoc.displayingTimeout = setTimeout(
+                myDoc.displayNextStats,
+                myDoc.data.displayingPeriod
             );
         }
         
