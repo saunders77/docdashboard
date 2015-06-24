bcc30825ebad552266b91b68e2c898e0034c2524
saunders77 <michaelsaunders@outlook.com>
2015-06-24 14:33:47 -0700

    Revert "Add clientid and docid to put response"
    This reverts commit ffa04c00a70398d91055f8956505d5157bdff60d.

--- FlaskWebProject/api_put.py
+++ FlaskWebProject/api_put.py
@@ -3,18 +3,10 @@ Routes and views for the flask application.
 """
 
 import FlaskWebProject.docdb as docdb
-from flask import jsonify
-from flask.json import loads
-from uuid import uuid4
 
 def api_put(params):
     client_id = params["clientid"]
     doc_id = params["docid"]
-    stats = loads(params["stats"])
+    stats = params["stats"]
 
-    if not client_id:
-        client_id = uuid4().hex
-    if not doc_id:
-        doc_id = uuid4().hex
-
-    return jsonify(clientid=client_id, docid = doc_id)
+    return "", 200
