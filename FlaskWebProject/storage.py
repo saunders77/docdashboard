4683615138d1f0435ae14ef421a37a2a11315b46
Mayank Kumar <mayank.alive@gmail.com>
2015-06-24 21:13:46 -0700

    Get data implemented

--- FlaskWebProject/storage.py
+++ FlaskWebProject/storage.py
@@ -1,7 +1,9 @@
 from azure.storage import TableService, Entity
 from flask.json import dumps
+import datetime
 
 table_name = 'testtable'
+doc_partition = 'docs'
 
 table_service = TableService(account_name = "docdash", account_key = "2f5QoMeIsCOsT6aitQqbcoH/ydSFnqwCxQIPpE0JEtQJR0o9Frqzc9btgLVZ9Y8wbhd7CRj9Q//X8qWku8fl3w==")
 table_service.create_table(table_name)
@@ -11,9 +13,23 @@ def add_data_to_doc(docid, data, time):
     e = Entity()
     e.PartitionKey = docid
     e.RowKey = "{:015}".format(int(time))
-    e.data = dumps(data)
+    e.charcount = data["charcount"]
     table_service.insert_or_replace_entity(table_name, e.PartitionKey, e.RowKey, e)
 
 def ensure_doc(docid):
-    table_service.insert_or_replace_entity(table_name, docid, docid, dict())
+    table_service.insert_or_replace_entity(table_name, doc_partition, docid, dict())
+
+def get_data_for_doc(docid):
+    timevalues = table_service.query_entities(table_name, "PartitionKey eq '" + docid + "'")
+    data = []
+    for timevalue in timevalues:
+        data.append([datetime.datetime.utcfromtimestamp(int(timevalue.RowKey)), timevalue.charcount])
+    return data
+
+def get_time_created(docid):
+    try:
+        doc = table_service.get_entity(table_name, doc_partition, docid)
+        return doc.Timestamp
+    except:
+        return None
 
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask import render_template, request
 from FlaskWebProject import app
 import FlaskWebProject.api_put as api_put
+import FlaskWebProject.api_get as api_get
 
 @app.route('/')
 @app.route('/home')
@@ -37,11 +38,13 @@ def about():
         message='Your application description page.'
     )
 
-@app.route('/api/<endpoint>', methods=["POST"])
+@app.route('/api/<endpoint>', methods=["GET", "POST"])
 def api(endpoint):
     """Responds to api requests"""
     if endpoint == "put":
         return api_put.api_put(request.values)
+    elif endpoint == "get":
+        return api_get.api_get(request.values)
 
 @app.route('/api/debug')
 def debug():
