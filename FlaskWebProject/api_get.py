959572a942208065aaeb844e2cca7ca2dc229227
Mayank Kumar <mayank.alive@gmail.com>
2015-06-24 21:39:26 -0700

    Missed api_get.py

---
+++ FlaskWebProject/api_get.py
@@ -0,0 +1,24 @@
+"""
+The Get API implementation
+"""
+
+from flask import jsonify, abort
+from flask.json import loads
+from time import time
+from uuid import uuid4
+
+import FlaskWebProject.storage as storage
+
+def api_get(params):
+    client_id = params["clientid"]
+    docs = loads(params["docs"])
+
+    if not client_id:
+        abort(404)
+
+    response = dict()
+    for docid, info in docs.items():
+        response[docid] = dict(
+            charcounts = storage.get_data_for_doc(docid),
+            timecreated = storage.get_time_created(docid))
+    return jsonify(dict(docs = response))
