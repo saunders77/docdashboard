d4ee0aabfc25abb683fcf1902a9cbd141e2e6635
Mayank Kumar <mayank.alive@gmail.com>
2015-06-25 17:07:20 -0700

    Deep copy data before charting

--- FlaskWebProject/static/scripts/charting.js
+++ FlaskWebProject/static/scripts/charting.js
@@ -26,14 +26,16 @@ function drawChart(data) {
 }
 
 function parseCharcountsForCharting(charcounts) {
+    var response = [];
     for (var i = 0; i < charcounts.length; ++i) {
         if (typeof charcounts[i][0] === "string") {
             charcounts[i][0] = Date.parse(charcounts[i][0]);
         } else if (charcounts[i][0].getTime) {
             charcounts[i][0] = charcounts[i][0].getTime();
         }
+        response.push([charcounts[i][0], charcounts[i][1]]);
     }
-    return charcounts;
+    return response;
 }
 
 //var data = JSON.parse("[[\"2015-06-25T20:55:30.911Z\",3],[\"2015-06-25T20:55:35.845Z\",14],[\"2015-06-25T20:56:00.646Z\",29],[\"2015-06-25T20:56:05.658Z\",29],[\"2015-06-25T20:56:10.640Z\",29],[\"2015-06-25T20:56:15.659Z\",29],[1435265776000,29],[\"2015-06-25T20:57:21.846Z\",29],[\"2015-06-25T20:57:24.381Z\",51],[\"2015-06-25T20:57:29.360Z\",99],[\"2015-06-25T20:57:34.364Z\",114]]");
